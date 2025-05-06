1d0fb74a4443f83f9a29ea59fcd2b667955cacd0
jerzy szajner <jerzy.szajner@gmail.com>
2025-05-06 12:05:24 +0200

    implement and provide routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Outlet } from "react-router-dom";
 import styles from "./App.module.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div>App component</div>;
+  return (
+    <>
+      <div>
+        <nav>Navbar</nav>
+      </div>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/pages/GameDetails/GameDetails.jsx
+++ src/pages/GameDetails/GameDetails.jsx
@@ -1,7 +1,7 @@
 import styles from "./GameDetails.module.css";
 
 const GameDetails = () => {
-  return <div></div>;
+  return <div>Game Details</div>;
 };
 
 export default GameDetails;
