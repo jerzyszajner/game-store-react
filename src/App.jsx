77b4b2432f7c4244a0a3784fdad8d54d89509777
jerzy szajner <jerzy.szajner@gmail.com>
2025-05-08 20:02:32 +0200

    implement navbar structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <>
-      <div>
-        <nav>Navbar</nav>
-      </div>
+      <header>
+        <Navbar />
+      </header>
       <main>
         <Outlet />
       </main>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,78 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { getAuthContext } from "../../context/AuthContext";
+import Button from "../Button/Button";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <nav></nav>;
+  const { user } = getAuthContext();
+  return (
+    <nav className={styles.navbar}>
+      {/* ----------------------------------------- */}
+      <div className={styles.firstRow}>
+        <div className={styles.logo}>
+          <img
+            src="/icons/playstation-controller.svg"
+            alt="PlayStation Controller Logo"
+          />
+        </div>
+        {/* ----------------------------------------- */}
+        <div className={styles.cartHamburgerMenu}>
+          {user ? (
+            <Button className={styles.signOutButton}>Sign out</Button>
+          ) : (
+            <Link to="/sign-in" className={styles.signInLink}>
+              Sign in
+            </Link>
+          )}
+          {user && (
+            <Link to="/profile" className={styles.profileButton}>
+              {user.imageUrl ? (
+                <img src={user.imageUrl} alt="User's profile picture" />
+              ) : (
+                <FontAwesomeIcon icon={faUser} className={styles.profileIcon} />
+              )}
+            </Link>
+          )}
+
+          <Link className={styles.cartButton}>
+            <FontAwesomeIcon icon={faCartPlus} className={styles.cartIcon} />
+          </Link>
+
+          <Button className={styles.hamburgerButton}>
+            <FontAwesomeIcon
+              icon={faBars}
+              className={styles.hamburgerMenuIcon}
+            />
+          </Button>
+        </div>
+      </div>
+      {/* ----------------------------------------- */}
+      <div className={styles.secondRow}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/games"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          Games
+        </NavLink>
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? styles.activeLink : "")}
+        >
+          Contact
+        </NavLink>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
